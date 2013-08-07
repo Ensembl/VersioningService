fe66c22368db0d1723374d9fa244eba45de8708c
ktaylor <ktaylor@ebi.ac.uk>
2013-08-07 13:55:30 +0100

    Adjusted to use LibXML::Reader and XPath to extract accessions. More to come.

--- lib/Bio/EnsEMBL/Mongoose/Parser/Swissprot.pm
+++ lib/Bio/EnsEMBL/Mongoose/Parser/Swissprot.pm
@@ -3,6 +3,7 @@ use Moose;
 use Moose::Util::TypeConstraints;
 
 use XML::LibXML::Reader;
+use XML::LibXML::XPathContext;
 use Bio::EnsEMBL::Mongoose::Parser::Record;
 
 
@@ -12,6 +13,10 @@ extends 'Bio::EnsEMBL::Mongoose::Parser::Parser';
 has 'record' => (
     is => 'rw',
     isa => 'Bio::EnsEMBL::Mongoose::Parser::Record',
+    lazy => 1,
+    default => sub {
+        return Bio::EnsEMBL::Mongoose::Parser::Record->new;
+    }
 );
 
 subtype 'XML::LibXML::Reader' => as 'Object';
@@ -31,21 +36,66 @@ has 'xml_reader' => (
     }
 );
 
-
-sub processNode {
-    my $self = shift;
-    my $reader = shift;
-    print $reader->name ."\n";
-}
+has 'xpath_context' => (
+    is => 'ro',
+    isa => 'XML::LibXML::XPathContext',
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        my $xpc = XML::LibXML::XPathContext->new();
+        $xpc->registerNs('uni','http://uniprot.org/uniprot');
+        return $xpc;
+    }
+);
 
 
 sub read_record {
     my $self = shift;
     my $reader = $self->xml_reader;
-    for (1..100) {
-        $reader->read;
-        $self->processNode($reader);
+    
+    my $record = $self->record;
+    
+    # fast-forward to next <entry> and extract attributes
+    unless ($reader->name && $reader->name eq "entry") {
+        $reader->nextElement("entry");
     }
+    $record->version($reader->getAttribute("version"));
+    
+
+    my $node = $reader->copyCurrentNode(1); # 1 = deep copy
+    $self->node_sieve($node);
+    
+    
+}
+
+sub node_sieve {
+    my $self = shift;
+    my $node = shift;
+    $self->accession($node);
+    
+    # note: can always fetch child nodes with $node->getElementsByTagName
+    return 1;
+}
+
+sub accession {
+    my $self = shift;
+    my $node = shift;
+    my $node_list = $self->xpath_context->findnodes('//uni:accession',$node);
+    
+    print $node_list->size."\n";
+    $node_list->foreach(sub {print $_->localname." ".$_->toString."\n"});
+    # First node is "primary accession"
+    my ($accession,@others) = $node_list->map(sub {$_->textContent});
+    $self->record->primary_accession($accession);
+    $self->record->accessions(\@others);
+}
+
+
+
+
+sub persist_record {
+    my $self = shift;
+    return 1;
 }
 
 1;

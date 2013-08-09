ff1b2fdd77c1f06d074bee12dd7b3f4ed6a9aaff
ktaylor <ktaylor@ebi.ac.uk>
2013-08-09 17:17:15 +0100

    Basic query functionality

---
+++ scripts/query_lucy.pl
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+
+use Lucy::Search::IndexSearcher;
+use Data::Dump::Color qw/dump/;
+
+my $query = shift;
+die "Specify query string" unless $query;
+
+my $lucy = Lucy::Search::IndexSearcher->new(
+    index => "/Users/ktaylor/projects/data/mongoose.index/"
+);
+
+my $hits = $lucy->hits(
+    query => $query,
+);
+
+
+print "Found: ".$hits->total_hits()."\n";
+while (my $hit = $hits->next) {
+    print dump($hit)."\n";
+    printf "%s %0.3f %s\n",$hit->{gene_name},$hit->get_score,$hit->{sequence}; 
+}

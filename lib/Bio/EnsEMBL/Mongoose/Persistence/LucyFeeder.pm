3e2b8bb48fe7da21f62972c32b89d06e7b04cc4a
ktaylor <ktaylor@ebi.ac.uk>
2013-08-09 12:59:17 +0100

    Simple record values persisted to Lucy index.

---
+++ lib/Bio/EnsEMBL/Mongoose/Persistence/LucyFeeder.pm
@@ -0,0 +1,71 @@
+package Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder;
+
+use Moose;
+use Moose::Util::TypeConstraints;
+
+use Lucy::Index::Indexer;
+use Lucy::Plan::Schema;
+use Lucy::Plan::StringType;
+use Lucy::Plan::BlobType;
+
+subtype 'Lucy::Plan::Schema' => as 'Object';
+
+has schema => (
+    is => 'ro',
+    isa => 'Lucy::Plan::Schema',
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        my $schema = Lucy::Plan::Schema->new;
+        my $lucy_str = Lucy::Plan::StringType->new(
+            sortable => 1,
+        );
+        my $lucy_blob = Lucy::Plan::BlobType->new(
+            stored => 1,
+        );
+    
+        $schema->spec_field( name => 'primary_accession', type => $lucy_str );
+        $schema->spec_field( name => 'accession', type => $lucy_str );
+        $schema->spec_field( name => 'taxon_id', type => $lucy_str );
+        $schema->spec_field( name => 'gene_name', type => $lucy_str );
+        $schema->spec_field( name => 'sequence', type => $lucy_blob );
+        return $schema;
+    },
+);
+
+has index => (
+    is => 'rw',
+    isa => 'Str',
+    default => '/Users/ktaylor/projects/data/mongoose.index',
+);
+
+has indexer => (
+    is => 'ro',
+    isa=> 'Lucy::Index::Indexer',
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        return Lucy::Index::Indexer->new(
+            schema => $self->schema,
+            index => $self->index,
+            create => 1,
+        );
+    }
+);
+
+sub load_record {
+    my $self = shift;
+    my $record = shift;
+    $self->indexer->add_doc({
+        primary_accession => $record->primary_accession,
+        taxon_id => $record->taxon_id,
+        gene_name => $record->gene_name,
+        sequence => $record->sequence,
+    });
+}
+
+
+
+__PACKAGE__->meta->make_immutable;
+
+1;
--- scripts/slurp_uniprot.pl
+++ scripts/slurp_uniprot.pl
@@ -2,13 +2,14 @@ use strict;
 use warnings;
 
 use Bio::EnsEMBL::Mongoose::Parser::Swissprot;
-#use IO::File;
-
-#my $fh = new IO::File "/Users/ktaylor/projects/data/uniprot_sprot.xml", "r" or die "unable to open. $@";
+use Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder;
 
 my $parser = Bio::EnsEMBL::Mongoose::Parser::Swissprot->new( source_file => "/Users/ktaylor/projects/data/uniprot_sprot.xml" );
+my $dumper = Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder->new();
+
 while ($parser->read_record) {
     my $record = $parser->record;
     printf "Main accession: %s, Gene name: %s, Taxon: %s, Sequence length: %s\n",
     $record->primary_accession,$record->gene_name, $record->taxon_id, length($record->sequence);
+    $dumper->load_record($record);
 };

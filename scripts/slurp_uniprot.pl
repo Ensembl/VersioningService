fa587c5be7ef63d5e5b434428bea8bf9467d0a36
ktaylor <ktaylor@ebi.ac.uk>
2013-09-03 17:21:45 +0100

    Added runtime info of progress, and forced a commit to try to limit memory consumption.

--- scripts/slurp_uniprot.pl
+++ scripts/slurp_uniprot.pl
@@ -18,11 +18,22 @@ use Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder;
 my $parser = Bio::EnsEMBL::Mongoose::Parser::Swissprot->new( source_file => $opts{data_location} );
 my $doc_store = Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder->new( index => $opts{index_location});
 
+my $buffer = 0;
+my $logger = Log::Log4perl->get_logger();
+
+$logger->info("Beginning to parse ".$opts{data_location});
+
 while ($parser->read_record) {
     my $record = $parser->record;
     printf "Main accession: %s, Gene name: %s, Taxon: %s\n",
     $record->primary_accession,$record->gene_name ? $record->gene_name : '', $record->taxon_id;
     $doc_store->store_record($record);
+    $buffer++;
+    if ($buffer % 100000 == 0) {
+        $logger->info("Committing 100000 records");
+        $doc_store->commit;
+    }
 };
 
 $doc_store->commit;
+$logger->info("Finished importing");

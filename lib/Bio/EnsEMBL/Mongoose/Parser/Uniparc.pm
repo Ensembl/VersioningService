5de806c1e3c6c90bf70c0d72ae4568b32a6a10e3
ktaylor <ktaylor@ebi.ac.uk>
2013-09-06 17:36:00 +0100

    Introducing a slightly quick and dirty parser of Uniparc data for retrospective Xref inserts.

---
+++ lib/Bio/EnsEMBL/Mongoose/Parser/Uniparc.pm
@@ -0,0 +1,103 @@
+package Bio::EnsEMBL::Mongoose::Parser::Swissprot;
+use Moose;
+use Moose::Util::TypeConstraints;
+
+use Bio::EnsEMBL::Mongoose::Persistence::Record;
+use Bio::EnsEMBL::Mongoose::Persistence::RecordXref;
+
+# Consumes Swissprot file and emits Mongoose::Persistence::Records
+with 'MooseX::Log::Log4perl';
+
+# 'uni','http://uniprot.org/uniprot'
+with 'Bio::EnsEMBL::Mongoose::Parser::XMLParser';
+
+around BUILDARGS => sub {
+    my ($orig, $class, %args) = @_;
+    # Add some default parameters to the parser
+    $args{short_namespace} = 'uni';
+    $args{namespace} = 'http://uniprot.org/uniparc';
+    $class->$orig(%args);
+    
+};
+
+sub read_record {
+    my $self = shift;
+    my $reader = $self->xml_reader;
+    
+    my $record = $self->record;
+    $self->clear_record;
+
+    my $read_state;
+    # fast-forward to next <entry> and extract attributes
+    
+    unless ($reader->name && $reader->name eq "entry") {
+        $read_state = $reader->nextElement("entry");
+        if ($read_state == 0) {return 0}
+    }
+    my $node = $reader->copyCurrentNode(1); # 1 = deep copy
+    # copyCurrentNode(1) does not spool the cursor to the end-tag!
+    
+    $self->node_sieve($node);
+    # move off <entry> node so unless(){} above can work next time.
+    $reader->next;
+    return $read_state;
+}
+
+sub node_sieve {
+    my $self = shift;
+    my $node = shift;
+    $self->log->debug('Parsing XML subtree.');
+    
+    $self->accession($node);
+    $self->sequence($node);
+    
+    return 1;
+}
+
+
+# Filtering out only Ensembl Xrefs for the purposes of the old xref pipeline.
+sub xrefs {
+    my $self = shift;
+    my $node = shift;
+    my $node_list = $self->xpath_context->findnodes('/uni:uniparc/uni:entry/uni:dbReference[@type="ENSEMBL"]',$node);
+    
+    $node_list->foreach( sub {
+        my $node = shift;
+        my @attributes = $node->attributes();
+        my ($source,$id);
+        foreach my $attr (@attributes) {
+            if ($attr->nodeName eq 'type') {$source = $attr->value}
+            elsif ($attr->nodeName eq 'id') {$id = $attr->value}
+        }
+        my $xref = Bio::EnsEMBL::Mongoose::Persistence::RecordXref->new(source => $source, id => $id);
+        
+        $self->record->add_xref($xref);
+    });
+}
+
+sub accession {
+    my $self = shift;
+    my $node = shift;
+    my $node_list = $self->xpath_context->findnodes('/uni:uniparc/uni:entry/uni:accession',$node);
+    
+    # First node is "primary accession"
+    my @accessions = $node_list->map(sub {$_->textContent});
+    $self->record->accessions(\@accessions);
+    $self->log->debug('Primary Accesion: '.$accessions[0]. ' and '.scalar(@accessions).' in total');
+}
+
+sub sequence {
+    my $self = shift;
+    my $node = shift;
+    my $node_list = $self->xpath_context->findnodes('/uni:uniparc/uni:entry/uni:sequence',$node);
+    my $seq_node = $node_list->next;
+    $self->record->sequence = $seq_node->textValue;
+    # Want checksum in the attribute
+    my @attributes = $seq_node->attributes();
+    
+    foreach my $attr (@attributes) {
+        if ($attr->nodeName eq 'checksum') {$self->record->checksum($attr->value)}
+        elsif ($attr->nodeName eq 'length') {$self->record->sequence_length($attr->value)}
+    }
+    
+}
---
+++ scripts/slurp_uniparc.pl
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+
+use FindBin qw/$Bin/;
+use Log::Log4perl;
+use Data::Dump::Color qw/dump/;
+use Devel::Size qw/total_size/;
+
+use Config::General;
+
+my $conf = Config::General->new("$Bin/../conf/uniparc.conf");
+my %opts = $conf->getall();
+
+Log::Log4perl::init("$Bin/../conf/logger.conf");
+
+use Bio::EnsEMBL::Mongoose::Parser::Swissprot;
+use Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder;
+
+my $parser = Bio::EnsEMBL::Mongoose::Parser::Swissprot->new( source_file => $opts{data_location} );
+my $doc_store = Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder->new( index => $opts{index_location});
+
+my $buffer = 0;
+my $logger = Log::Log4perl->get_logger();
+
+$logger->info("Beginning to parse ".$opts{data_location});
+
+while ($parser->read_record) {
+    my $record = $parser->record;
+    #printf "Main accession: %s, Gene name: %s, Taxon: %s\n",
+    #$record->primary_accession,$record->gene_name ? $record->gene_name : '', $record->taxon_id;
+    $doc_store->store_record($record);
+    $buffer++;
+    if ($buffer % 10000 == 0) {
+        $logger->info("Committing 10000 records");
+        $logger->info("MEM: ".`ps -p $$ -h -o rss=`);
+        
+#        $doc_store->commit;
+#        $doc_store = Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder->new( index => $opts{index_location});
+#        $logger->info("MEM2: ".`ps -p $$ -h -o rss=`);
+    }
+};
+
+$doc_store->commit;
+$logger->info("Finished importing");

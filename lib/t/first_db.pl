89c47f92fc038d77a945cf4464d9e7ef41f4a062
mr6 <mr6_@ebi.ac.uk>
2013-12-13 10:15:45 +0000

    value column in resources table has been expanded, values could be big added uri column in version table will store where the file/index for that specific version should be/is stored

---
+++ lib/t/first_db.pl
@@ -0,0 +1,45 @@
+use strict;
+use warnings;
+
+use Test::More;
+use Test::Exception;
+
+use Bio::EnsEMBL::Versioning::DB;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+
+my $dba = Bio::EnsEMBL::DBSQL::DBAdaptor->new(
+-host => 'ens-production',
+-species => 'multi',
+-group => 'versioning',
+-user => 'ensadmin',
+-pass => 'ensembl',
+-dbname => 'versioning_db'
+);
+Bio::EnsEMBL::Versioning::DB->register_DBAdaptor($dba);
+
+require Bio::EnsEMBL::Versioning::Manager::Version;
+require Bio::EnsEMBL::Versioning::Manager::Process;
+require Bio::EnsEMBL::Versioning::Manager::Source;
+require Bio::EnsEMBL::Versioning::Manager::SourceGroup;
+require Bio::EnsEMBL::Versioning::Manager::SourceDownload;
+require Bio::EnsEMBL::Versioning::Manager::Resources;
+require Bio::EnsEMBL::Versioning::Manager::Run;
+
+my $source = Bio::EnsEMBL::Versioning::Object::Source->new(name => 'UniProt');
+$source->source_group(name => 'UniProtGroup');
+$source->save();
+
+my $resource = Bio::EnsEMBL::Versioning::Object::Resources->new(name => 'uniprot_ftp', type => 'ftp', value => 'ftp://ftp.ebi.ac.uk/pub/databases/uniprot/knowledgebase/uniprot_trembl.dat.gz');
+$resource->source_download(module => 'UniProtParser');
+$resource->source_download->source(name => 'UniProt');
+$resource->save();
+
+my $version_resource = Bio::EnsEMBL::Versioning::Object::Resources->new(name => 'uniprot_version', type => 'ftp', value => 'ftp://ftp.ebi.ac.uk/pub/databases/uniprot/knowledgebase/reldate.txt');
+$resource->source_download(module => 'UniProtParser');
+$resource->source_download->source(name => 'UniProt');
+
+my $version = Bio::EnsEMBL::Versioning::Object::Version->new(version => '2013_12', record_count => 49243530, is_current => 1, uri => '/lustre/scratch110/ensembl/mr6/Uniprot/203_12/uniprot.txt');
+$version->source(name => 'Uniprot');
+$version->source->source_group(name => 'UniprotGroup');
+$version->save();
+

29e5b928e28a06123657bc526fddb9767932d3b2
Kieron Taylor <ktaylor@ebi.ac.uk>
2015-01-20 13:24:58 +0000

    Update FTP role to be able to access FTP sites that require logins other than the defaults. Also remove dependency on Ensembl Core. Test made more tidy and worthwhile.

--- lib/Bio/EnsEMBL/Versioning/Pipeline/Downloader/FTPClient.pm
+++ lib/Bio/EnsEMBL/Versioning/Pipeline/Downloader/FTPClient.pm
@@ -2,78 +2,117 @@ package Bio::EnsEMBL::Versioning::Pipeline::Downloader::FTPClient;
 
 use Moose::Role;
 
-use Bio::EnsEMBL::Utils::Net qw/do_FTP_to_file/;
 use Net::FTP;
 use URI;
 use Cwd;
 use File::Basename;
 use Bio::EnsEMBL::Mongoose::NetException;
 use Try::Tiny;
+use Method::Signatures;
+
+has ftp => (
+  isa => 'Net::FTP',
+  is => 'rw',
+  predicate => 'connected_to_ftp',
+);
+
+method connect_to_ftp_site ($url, $user, $password){
+  my $ftp = Net::FTP->new($url->host);
+  $ftp->login($user,$password) or Bio::EnsEMBL::Mongoose::NetException->throw("Cannot log into FPT site $url with $user:$password");
+  $self->ftp($ftp);
+}
 
 =head2 get_ftp_files
 
   Arg [1]     : The URL to find the files at
   Arg [2]     : Filename pattern. This can be explicit or a regex pattern.
   Arg [3]     : [Optional] download path, somewhere to force the downloaded data to go
-  Example     :
+  Arg [4]     : User name for awkward FTP sites
+  Arg [5]     : Password for even more awkward FTP sites
+
   Description : Given a ftp resource, returns the corresponding file or files
   Returntype  : listref of File paths to the downloaded resources
-  Exceptions  : Throw on FTP get failed.
+  Exceptions  : Throw on FTP get failed, or insufficient arguments.
   Caller      : internal
 
 =cut
 
-
-sub get_ftp_files
+method get_ftp_files (
+    Str $host_URL, 
+    Str $filename_pattern, 
+    Str $path = cwd(),
+    Str $user = 'anonymous', 
+    Str $password = '-anonymous@')
 {
-  my $self = shift;
-  my $host_URL = shift;
-  my $filename_pattern = shift;
-  my $path = shift;
-
-  $path ||= cwd();
   unless ($host_URL && $filename_pattern) { Bio::EnsEMBL::Mongoose::NetException->throw("Insufficient arguments to download");}
   my $result;
   my $uri = URI->new($host_URL);
-  
+  unless ($self->connected_to_ftp) {
+    $self->connect_to_ftp_site($uri,$user,$password);
+  }
+  $self->ftp->cwd($uri->path()) or Bio::EnsEMBL::Mongoose::NetException->throw("Unable to change to remote directory ".$uri->path." on server ".$uri->host);
   my @candidates = @{ $self->ls_ftp_dir($uri) };
   my @files;
   foreach my $file (@candidates) {
     if ($file =~ /$filename_pattern/) {
-      print "Pattern matched $file.\n";
+      print "Pattern matched $file\n";
       my $download_name = $path . '/' . $file;
-      try { 
-        do_FTP_to_file($host_URL.'/'.$file, undef, undef, $download_name); 
-      }
-      catch {
-        Bio::EnsEMBL::Mongoose::NetException->throw($_);
-      };
-      push @files, $download_name;
+      
+      my $success = _retry_sleep( sub {
+        my $response = $self->ftp->get($file);
+        unless ($response) {print $self->ftp->message."\n"; return}
+          else {return $response}
+      }, 2   );
+
+      if ($success) { push @files, $download_name} else { Bio::EnsEMBL::Mongoose::NetException->throw("Incomplete download for $host_URL $filename_pattern") }; 
     }
   }
   return \@files;
 }
 
-sub ls_ftp_dir
-{
-  my $self = shift;
-  my $uri = shift;
+method ls_ftp_dir ($uri, $user = 'anonymous', $password = '-anonymous@') {
   unless ($uri->isa('URI')) { $uri = URI->new($uri) }
-
-  my $ftp = Net::FTP->new($uri->host());
-  if (!$ftp->login('anonymous', '-anonymous@')) {
-    Bio::EnsEMBL::Mongoose::NetException->throw('Cannot log into FTP host '.$uri->host);
+  unless ($self->connected_to_ftp) {
+    $self->connect_to_ftp_site($uri,$user,$password);
   }
-  if (!$ftp->cwd($uri->path())) {
+  if (!$self->ftp->cwd($uri->path())) {
     Bio::EnsEMBL::Mongoose::NetException->throw('Cannot navigate to path on FTP host '.$uri->path);
   }
-  my $files = $ftp->ls();
+  my $files = $self->ftp->ls();
   return $files;
 }
 
-# pass-through method to allow Role methods to cross over
-sub do_FTP {
-  return Bio::EnsEMBL::Utils::Net::do_FTP(@_);
+method get_timestamp ($uri,$file,$user = 'anonymous' ,$password = '-anonymous@') {
+  unless ($uri->isa('URI')) { $uri = URI->new($uri) }
+  unless ($self->connected_to_ftp) {
+    $self->connect_to_ftp_site($uri,$user,$password);
+  }
+  $self->ftp->cwd($uri->path()) or Bio::EnsEMBL::Mongoose::NetException->throw("Unable to change to remote directory ".$uri->path." on server ".$uri->host);
+  my $time = $self->ftp->mdtm($file);
+  return $time;
+}
+
+# taken from Ensembl Utils/Net
+sub _retry_sleep {
+  my ($callback, $total_attempts, $sleep) = @_;
+  $total_attempts ||= 1;
+  $sleep ||= 0;
+  my $response;
+  my $retries = 0;
+  my $fail = 1;
+  while($retries <= $total_attempts) {
+    $response = $callback->();
+    if(defined $response) {
+      $fail = 0;
+      last;
+    }
+    $retries++;
+    Time::HiRes::sleep($sleep);
+  }
+  if($fail) {
+    Bio::EnsEMBL::Mongoose::NetException->throw("Could not request remote resource after $total_attempts attempts");
+  }
+  return $response;
 }
 
 1;
--- t/downloader.t
+++ t/downloader.t
@@ -11,11 +11,13 @@ $refseq->file_pattern('complete\.55\.bna.*');
 
 my $version = $refseq->get_version;
 # cmp_ok($version,'==',69,'Current RefSeq version is as expected'); # Not version independent obviously
-note("Downloading Refseq version: ".$version);
+note("Downloading Refseq version: ".$version." to ".cwd());
 
 my $result = $refseq->download_to(cwd());
 # is($result->[0],cwd().'/complete.55.1.genomic.fna.gz','Download of single Refseq file successful');
 is($result->[0],cwd().'/complete.55.bna.gz','Download of only matching Refseq file successful');
 # is($result->[0],cwd().'/complete.55.genomic.gbff.gz','Download of third Refseq file successful');
+ok(-e cwd().'/complete.55.bna.gz','File was actually written to disk');
 
+unlink(cwd().'/complete.55.bna.gz');
 done_testing;

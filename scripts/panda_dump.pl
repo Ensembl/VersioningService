49c8657f99dd59b9380e6c2cb6b7e68b558d3609
Kieron Taylor <ktaylor@ebi.ac.uk>
2015-01-21 11:20:44 +0000

    Marginal improvements to example dumping script

--- scripts/panda_dump.pl
+++ scripts/panda_dump.pl
@@ -4,14 +4,18 @@ use IO::File;
 use Log::Log4perl;
 use Bio::EnsEMBL::Mongoose::IndexSearch;
 use Bio::EnsEMBL::Mongoose::Persistence::QueryParameters;
+use Bio::EnsEMBL::Mongoose::Serializer::EnsemblRDF;
+
 Log::Log4perl::init("$ENV{MONGOOSE}/conf/logger.conf");
 
-my $base_path = $ENV{'PWD'};
+my $base_path = $ENV{'HOME'}.'/projects/';
 my $fh = IO::File->new("$base_path/panda_triples",'w');
+
+my $species = 'ailuropoda melanoleuca';
 
 my $extractor = Bio::EnsEMBL::Mongoose::IndexSearch->new(
   handle => $fh, 
-  species => 'ailuropoda melanoleuca', 
+  species => $species, 
   output_format => 'RDF', 
   source => 'UniProtSwissProt'
 );
@@ -19,3 +23,6 @@ my $extractor = Bio::EnsEMBL::Mongoose::IndexSearch->new(
 $extractor->work_with_index(source =>'UniProtSwissProt');
 $extractor->get_records;
 
+
+my $ens_writer = Bio::EnsEMBL::Mongoose::Serializer::EnsemblRDF->new(species => $species, handle => $fh);
+$ens_writer->print_record;

7fe59cfa6744c3f64444a69dd12b2bd8ba62df1a
Kieron Taylor <ktaylor@ebi.ac.uk>
2015-01-26 16:10:08 +0000

    New downloader module for HGNC data.

---
+++ lib/Bio/EnsEMBL/Versioning/Pipeline/Downloader/HGNC.pm
@@ -0,0 +1,71 @@
+=head1 LICENSE
+
+Copyright [1999-2013] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+=head1 NAME
+
+Bio::EnsEMBL::Versioning::Pipeline::Downloader::HGNC
+
+=head1 DESCRIPTION
+
+A module for HGNC downloads. Original method:
+
+wget=>http://www.genenames.org/cgi-bin/download
+?title=HGNC+output+data&hgnc_dbtag=on&col=gd_hgnc_id&
+col=gd_status&col=gd_ccds_ids&status=Approved&status_opt=2&level=pri&=on&where=&order_by=gd_app_sym_sort&limit=&
+format=text&submit=submit&.cgifields=&.cgifields=level&.cgifields=chr&.cgifields=status&.cgifields=hgnc_dbtag,host=>ens-livemirror,dbname=>ccds_human_77
+
+This data is now available from a REST endpoint based on Solr.
+
+=cut
+
+package Bio::EnsEMBL::Versioning::Pipeline::Downloader::HGNC;
+
+use Moose;
+use Try::Tiny;
+use Time::gmtime;
+extends 'Bio::EnsEMBL::Versioning::Pipeline::Downloader';
+
+has uri => (
+  isa => 'Str', 
+  is => 'ro',
+  default => 'http:/rest.genenames.org/',
+);
+
+with 'MooseX::Log::Log4perl', 'Bio::EnsEMBL::Versioning::Pipeline::Downloader::RESTClient';
+
+# HGNC continually updates its data, no releases. Therefore the version is a timestamp of now
+sub get_version
+{
+  my $self = shift;
+  my $gmt = gmtime();
+  my $time = sprintf "%04u%02s%02s",$gmt->year + 1900,$gmt->month,$gmt->day;
+}
+
+sub _get_remote {
+  my $self = shift;
+  my $path = shift; # path is already checked as valid.
+
+  $self->call(host => $self->uri, path => 'fetch/status/Approved', file_path => $path, file_name => 'hgnc', retry_delay => 30);
+
+  unless ($result) { Bio::EnsEMBL::Mongoose::NetException->throw("Failed to download HGNC file") }
+  return ;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;

580530ce313c5b2f79473d3fcc26b0f98c2d7be5
Kieron Taylor <ktaylor@ebi.ac.uk>
2015-08-28 12:00:34 +0100

    License updates

--- scripts/mfetcher_demo.pl
+++ scripts/mfetcher_demo.pl
@@ -1,3 +1,20 @@
+# Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#      http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Demo script to demonstrate "Mfetch-like" behaviour of the Swissprot index.
+# Takes a species name and writes out FASTA for that species.
+
 use strict;
 use warnings;
 
@@ -11,7 +28,7 @@ use Log::Log4perl;
 Log::Log4perl::init("$ENV{MONGOOSE}/conf/logger.conf");
 
 my $query = shift;
-die "Specify query string" unless $query;
+die "Specify query string e.g. 'mus musculus'" unless $query;
 
 my $mfetcher = Bio::EnsEMBL::Mongoose::IndexSearch->new();
 $mfetcher->work_with_index(source => 'Swissprot');

34844d1279c9268d03bc22644c9cb8953957089a
Kieron Taylor <ktaylor@ebi.ac.uk>
2015-08-28 11:43:47 +0100

    Drop version, so it just goes with latest

--- scripts/mfetcher_demo.pl
+++ scripts/mfetcher_demo.pl
@@ -14,7 +14,7 @@ my $query = shift;
 die "Specify query string" unless $query;
 
 my $mfetcher = Bio::EnsEMBL::Mongoose::IndexSearch->new();
-$mfetcher->work_with_index('Swissprot','2015_08');
+$mfetcher->work_with_index(source => 'Swissprot');
 
 my $params = Bio::EnsEMBL::Mongoose::Persistence::QueryParameters->new(
     species_name => $query

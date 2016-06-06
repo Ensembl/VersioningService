1f1d6d392e682dc9eacf03d6b5d0cb96c81b98f7
Kieron Taylor <ktaylor@ebi.ac.uk>
2016-06-06 16:06:22 +0100

    Rename package to match file

--- lib/Bio/EnsEMBL/Mongoose/Parser/SwissprotFaster.pm
+++ lib/Bio/EnsEMBL/Mongoose/Parser/SwissprotFaster.pm
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-package Bio::EnsEMBL::Mongoose::Parser::Swossprit;
+package Bio::EnsEMBL::Mongoose::Parser::SwissprotFaster;
 use Moose;
 use Moose::Util::TypeConstraints;
 

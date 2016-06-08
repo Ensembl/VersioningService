41994566f426b17c456d386338d5a2630f0bf7b9
Kieron Taylor <ktaylor@ebi-003.ebi.ac.uk>
2016-06-08 13:53:22 +0100

    Make sample script a bit more useful

--- scripts/slurp_uniprot.pl
+++ scripts/slurp_uniprot.pl
@@ -2,43 +2,41 @@ use strict;
 use warnings;
 
 use Log::Log4perl;
-use Data::Dump::Color qw/dump/;
-use Devel::Size qw/total_size/;
+use Time::HiRes qw(gettimeofday tv_interval);
+my $data_location = shift;
+$data_location ||= '/gpfs/nobackup/ensembl/ktaylor/uniprot_trembl.xml.gz';
 
-use Config::General;
+my $index_location = shift;
+$index_location ||= '/gpfs/nobackup/ensembl/ktaylor/index/';
 
-my $conf = Config::General->new("$ENV{MONGOOSE}/conf/swissprot.conf");
-my %opts = $conf->getall();
 
 Log::Log4perl::init("$ENV{MONGOOSE}/conf/logger.conf");
 
-use Bio::EnsEMBL::Mongoose::Parser::Swissprot;
+use Bio::EnsEMBL::Mongoose::Parser::SwissprotFaster;
 use Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder;
 #use Bio::EnsEMBL::Mongoose::Persistence::FlatFileFeeder;
 
-my $parser = Bio::EnsEMBL::Mongoose::Parser::Swissprot->new( source_file => $opts{data_location} );
-my $doc_store = Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder->new( index => $opts{index_location});
+my $parser = Bio::EnsEMBL::Mongoose::Parser::SwissprotFaster->new( source_file => $data_location );
+my $doc_store = Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder->new( index => $index_location);
 #my $doc_store = Bio::EnsEMBL::Mongoose::Persistence::FlatFileFeeder->new( index => $opts{index_location});
 my $buffer = 0;
 my $logger = Log::Log4perl->get_logger();
 
-$logger->info("Beginning to parse ".$opts{data_location});
+$logger->info("Beginning to parse ".$data_location);
 
 while ($parser->read_record) {
+    my $start_time = [gettimeofday];
     my $record = $parser->record;
-    #printf "Main accession: %s, Gene name: %s, Taxon: %s\n",
-    #$record->primary_accession,$record->gene_name ? $record->gene_name : '', $record->taxon_id;
     $doc_store->store_record($record);
     $buffer++;
     if ($buffer % 100000 == 0) {
-        $logger->info("Committing 100000 records");
-#        $logger->info("Parser:".total_size $parser);
-        $logger->info("MEM: ".`ps --no-headers -p $$ -o rss=`);
         $doc_store->commit;
-        $doc_store = Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder->new( index => $opts{index_location});
-#        $logger->info("MEM2: ".`ps -p $$ -h -o rss=`);
+        $doc_store = Bio::EnsEMBL::Mongoose::Persistence::LucyFeeder->new( index => $index_location);
+        my $interval = tv_interval($start_time, [gettimeofday]);
+        printf "%.2f records per second\n",100000/$interval;
     }
 };
 
 $doc_store->commit;
 $logger->info("Finished importing");
+print "Finished with $buffer records\n";

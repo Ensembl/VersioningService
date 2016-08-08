9eef87e003b028879b6e0a9c1f41804ed80bc2ce
Kieron Taylor <ktaylor@ebi.ac.uk>
2016-08-08 17:44:03 +0100

    Apply boilerplate copyright notice to match other Ensembl repositories

--- lib/Bio/EnsEMBL/RDF/EnsemblToIdentifierMappings.pm
+++ lib/Bio/EnsEMBL/RDF/EnsemblToIdentifierMappings.pm
@@ -1,6 +1,7 @@
 =head1 LICENSE
 
-Copyright [1999-2016] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-=head1 NAME
+=cut
 
     EnsemblToIdentifierMappings - Module to help map Ensembl Xrefs to Identifiers.org namespaces
 

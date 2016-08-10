a026c4448f1845898d8e88e5df21fe69d6a08da3
Kieron Taylor <ktaylor@ebi.ac.uk>
2016-08-10 14:20:58 +0100

    Fix broken licensing

--- lib/Bio/EnsEMBL/RDF/EnsemblToIdentifierMappings.pm
+++ lib/Bio/EnsEMBL/RDF/EnsemblToIdentifierMappings.pm
@@ -15,9 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-=cut
-
-    EnsemblToIdentifierMappings - Module to help map Ensembl Xrefs to Identifiers.org namespaces
+EnsemblToIdentifierMappings - Module to help map Ensembl Xrefs to Identifiers.org namespaces
 
 =head1 SYNOPSIS
   my $mapper = Bio::EnsEMBL::RDF::EnsemblToIdentifierMappings->new($config_file);

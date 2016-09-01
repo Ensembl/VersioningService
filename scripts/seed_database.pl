c96689e05e3e0fe36ddb1f58cc5b3be63f7be361
Kieron Taylor <ktaylor@ebi.ac.uk>
2016-09-01 17:03:35 +0100

    Update database seeding-script to reflect changed MIM and Uniprot parsers

--- scripts/seed_database.pl
+++ scripts/seed_database.pl
@@ -23,12 +23,12 @@ Log::Log4perl::init("$ENV{MONGOOSE}/conf/logger.conf");
 
 my $broker = Bio::EnsEMBL::Versioning::Broker->new(create => 1,config_file => "$ENV{MONGOOSE}/conf/manager.conf");
 
-$broker->add_new_source('Uniprot/SWISSPROT','Uniprot',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::UniProtSwissProt','Bio::EnsEMBL::Mongoose::Parser::Swissprot');
+$broker->add_new_source('Uniprot/SWISSPROT','Uniprot',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::UniProtSwissProt','Bio::EnsEMBL::Mongoose::Parser::SwissprotFaster');
 $broker->add_new_source('UniParc','Uniprot',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::UniProtUniParc','Bio::EnsEMBL::Mongoose::Parser::Uniparc');
-$broker->add_new_source('Uniprot/SPTREMBL','Uniprot',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::UniProtTrembl','Bio::EnsEMBL::Mongoose::Parser::Swissprot');
+$broker->add_new_source('Uniprot/SPTREMBL','Uniprot',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::UniProtTrembl','Bio::EnsEMBL::Mongoose::Parser::SwissprotFaster');
 $broker->add_new_source('RefSeq','RefSeq',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::RefSeq','Bio::EnsEMBL::Mongoose::Parser::RefSeq');
 $broker->add_new_source('MIM','MIM',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::MIM','Bio::EnsEMBL::Mongoose::Parser::MIM');
-$broker->add_new_source('mim2gene','MIM',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::MIM2Gene','Bio::EnsEMBL::Mongoose::Parser::MIM2Gene');
+$broker->add_new_source('mim2gene','MIM',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::MIM2GeneMedGen','Bio::EnsEMBL::Mongoose::Parser::MIM2GeneMedGen');
 $broker->add_new_source('HGNC','HGNC',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::HGNC','Bio::EnsEMBL::Mongoose::Parser::HGNC');
 
 print "Created basic Xref working set of sources. Now enable the update pipeline to allow it to download the first copy of each source\n";

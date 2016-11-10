46f4c9c54bcf52a718f71035c202f0fb07053585
Kieron Taylor <ktaylor@ebi.ac.uk>
2016-11-10 14:29:45 +0000

    [ENSCORESW-2012] Establish simple SPARQL query infrastructure for common queries against a triplestore.

---
+++ lib/Bio/EnsEMBL/Mongoose/Persistence/TriplestoreQuery.pm
@@ -0,0 +1,133 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head2 DESCRIPTION
+
+TriplestoreQuery - a query object to run general and pre-cooked SPARQL queries against an HTTP-accessible triplestore
+
+=cut
+
+package Bio::EnsEMBL::Mongoose::Persistence::TriplestoreQuery;
+use Moose;
+use RDF::Query::Client;
+use Bio::EnsEMBL::RDF::RDFlib qw/compatible_name_spaces/;
+use Bio::EnsEMBL::Mongoose::DBException;
+
+has triplestore_url => (
+  is => 'rw',
+  isa => 'Str',
+  default => 'http://127.0.0.1:8890/sparql'
+);
+
+has result_set => (
+    is => 'rw',
+    lazy => 1,
+    default => sub {
+        
+    }
+);
+# name of named graph in triplestore
+has graph => (
+  is => 'rw',
+  isa => 'Maybe[Str]',
+);
+
+with 'Bio::EnsEMBL::Mongoose::Persistence::Query';
+
+# returns a result iterator
+sub query {
+  my $self = shift;
+  my $query = shift;
+  my $config = $self->query_parameters;
+  # TODO: implement support for query modification with parameters
+  #       species_name = graph choice, result_size = LIMIT, ids mapped to Ensembl URIs?
+  my $sparql = RDF::Query::Client->new($query);
+  my $result_iterator = $sparql->execute($self->triplestore_url);
+  my $error = $sparql->error();
+  if ($error) { Bio::EnsEMBL::Mongoose::DBException->throw($error) }
+  $self->result_set($result_iterator);
+}
+
+sub next_result {
+  my $self = shift;
+  $self->result_set->next;
+}
+
+# Query graph for an ID and all related xrefs (outbound links), returns a list of names
+sub recurse_xrefs {
+  my $self = shift;
+  my $id = shift;
+  my $result_hash = {};
+  my $graph_name = $self->generate_graph_name;
+  my $query = sprintf qq(%s\nSELECT DISTINCT ?xref_label %s {
+    ?o dc:identifier "%s" .
+    ?o term:refers-to+ ?e .
+    ?e dc:identifier ?xref_label.
+    }), compatible_name_spaces(),$graph_name,$id;
+  # print $query."\n";
+  $self->query($query);
+
+  my @xrefs = ();
+  return unless defined $self->result_set;
+  # map result objects into array of xref labels
+  while (my $result = $self->next_result) {
+    # print Dumper $result_iterator;
+    my $string = $result->{xref_label}->as_string;
+    $string =~ s/"//g;
+    push @xrefs,$string;
+  }
+  return \@xrefs;
+}
+
+# Query graph for all inbound links to a single known node
+sub get_all_linking_xrefs {
+  my $self = shift;
+  my $id = shift;
+  my $result_hash = {};
+  my $graph_name = $self->generate_graph_name;
+  my $query = sprintf qq(%s\nSELECT DISTINCT ?xref_label %s {
+    ?ref dc:identifier "%s" .
+    ?o term:refers-to+ ?ref .
+    ?o dc:identifier ?xref_label.
+    }), compatible_name_spaces(),$graph_name,$id;
+  # print $query."\n";
+  $self->query($query);
+
+  my @xrefs = ();
+  return unless defined $self->result_set;
+  # map result objects into array of xref labels
+  while (my $result = $self->next_result) {
+    # print Dumper $result_iterator;
+    my $string = $result->{xref_label}->as_string;
+    $string =~ s/"//g;
+    push @xrefs,$string;
+  }
+  return \@xrefs;
+}
+
+sub generate_graph_name {
+  my $self = shift;
+  my $graph_name = $self->graph;
+  if ($graph_name) {
+    $graph_name = "FROM <".$graph_name.">" ;
+  } else {
+    $graph_name = "";
+  }
+  return $graph_name;
+}
+
+1;
---
+++ t/triplestore_query.t
@@ -0,0 +1,66 @@
+use strict;
+use Test::More;
+use Test::Differences;
+use Test::Deep;
+use List::Compare;
+use Test::MockObject::Extends;
+use Bio::EnsEMBL::Mongoose::Persistence::TriplestoreQuery;
+use RDF::Trine;
+use RDF::Query;
+use Data::Dumper;
+
+my $query_agent = Bio::EnsEMBL::Mongoose::Persistence::TriplestoreQuery->new();
+my $test_data = "$ENV{MONGOOSE}/t/data/test_graph.ttl";
+
+our $store = RDF::Trine::Store::Memory->new();
+our $model = RDF::Trine::Model->new($store);
+my $parser = RDF::Trine::Parser->new('turtle');
+$parser->parse_file_into_model("http://everything/",$test_data,$model);
+ok(1, 'Test data loaded');
+
+# Graph is a simple a->x->b->x2->c with Subject labels
+
+cmp_ok($model->size, '==', 9, "Check all triples have been imported");
+$query_agent = Test::MockObject::Extends->new($query_agent);
+
+
+my $mock_query = sub {
+  my $self = shift;
+  my $query = shift;
+  # note Dumper $query;
+  my $query_obj = RDF::Query->new($query);
+  my $iterator = $query_obj->execute($model);
+  $query_agent->result_set($iterator);
+  return $iterator;
+};
+
+$query_agent->mock(query => $mock_query);
+$query_agent->graph("http://everything/");
+# Prove basic querying is functional
+$query_agent->query(sprintf qq(%s\nSELECT ?s ?p ?o {
+    ?s ?p ?o .
+    }), $query_agent->compatible_name_spaces());
+
+my $counter = 0;
+while (my $hit = $query_agent->result_set->next) {
+  $counter++;
+  # note $hit->{s}, $hit->{p}, $hit->{o};
+}
+cmp_ok($counter, '==', 9, 'All triples extracted via generic query');
+
+
+# Prove we can ask sensible questions of an xref graph
+
+my $id_list = $query_agent->recurse_xrefs('Subject a');
+is_deeply($id_list, ['Subject b','Subject c'], 'a links to b and c');
+
+my $id_list = $query_agent->recurse_xrefs('Subject b');
+is_deeply($id_list, ['Subject c'], 'b links only to a');
+# note Dumper $id_list;
+
+my $id_list = $query_agent->get_all_linking_xrefs('Subject c');
+is_deeply($id_list, ['Subject a','Subject b'], 'c is linked to by a and b');
+
+
+
+done_testing();

6abb963cd98eae899f9365894bb2efd440fe7640
Alessandro Vullo <avullo@ebi.ac.uk>
2016-12-19 17:21:32 +0000

    [ENSCORESW-2071]. Delving into parsing UCSC content, not incorporating lots of information.

--- lib/Bio/EnsEMBL/Mongoose/Parser/UCSC.pm
+++ lib/Bio/EnsEMBL/Mongoose/Parser/UCSC.pm
@@ -20,7 +20,6 @@ limitations under the License.
 
 =head1 DESCRIPTION
 
-
 =cut
 
 package Bio::EnsEMBL::Mongoose::Parser::UCSC;
@@ -33,6 +32,13 @@ use Bio::EnsEMBL::Mongoose::Persistence::RecordXref;
 
 with 'Bio::EnsEMBL::Mongoose::Parser::Parser', 'MooseX::Log::Log4perl';
 
+#
+# TODO
+# - taxon ID?
+# - the original parser creates a coordinate-based xref to handle
+#   coordinates info in the UCSC file.
+#   What should we do? Specialise Record class with additional attributes?
+#
 sub read_record {
   my $self = shift;
   $self->clear_record;
@@ -45,16 +51,93 @@ sub read_record {
   chomp($content);
   $content =~ s/\s*$//;
 
-  # my ($antibody, $antibody_id, $ensembl_peptide_id, $link) = split /,/, $content;
-  # Bio::EnsEMBL::Mongoose::IOException->throw ('Insufficient data in HPA line: ' . $content)
-  #     unless $antibody and $antibody_id and $ensembl_peptide_id;
-  # Bio::EnsEMBL::Mongoose::IOException->throw ("Wrong Ensembl peptide ID in HPA line: $ensembl_peptide_id")
-  #     if $ensembl_peptide_id !~ /^ENSP0{5}[0-9]{6}/;
+    # Each line will have the following tab-delimited fields:
+    # 0.    name        (UCSC stable ID)
+    # 1.    chrom       (chromosome name, a la UCSC)
+    # 2.    strand      (plus or minus)
+    # 3.    txStart     (transcript start)
+    # 4.    txEnd       (transcript end)
+    # 5.    cdsStart    (CDS start)
+    # 6.    cdsEnd      (CDS end)
+    # 7.    exonCount   (number of exons in transcript)
+    # 8.    exonStarts  (comma-separated list of exon start positions)
+    # 9.    exonEnds    (comma-separated list of exon end positions)
+    # 10.   proteinID   (cross reference to a protein ID, e.g. UniProt)
+    # 11.   alignID     (not sure what this is right now)
 
-  # $record->id($antibody_id);
-  # $record->display_label($antibody);
-  # $record->add_xref(Bio::EnsEMBL::Mongoose::Persistence::RecordXref->new(source => 'Ensembl', creator => 'HPA', id => $ensembl_peptide_id));
+  my @fields = split( /\t/, $content );
+  my ( $name,
+       $chrom,
+       $strand,
+       $txStart,
+       $txEnd,
+       $cdsStart,
+       $cdsEnd,
+       $exonStarts,
+       $exonEnds ) = @fields[ 0 .. 6, 8, 9];
+  my @protein_ids = @fields[10 .. $#fields];
 
+  # UCSC uses slightly different chromosome names, at least for
+  # human and mouse, so chop off the 'chr' in the beginning.  We do
+  # not yet translate the names of the special chromosomes, e.g.
+  # "chr6_cox_hap1" (UCSC) into "c6_COX" (Ensembl).
+  $chrom =~ s/^chr//;
+
+  # They also use '+' and '-' for the strand, instead of -1, 0, or 1.
+  if    ( $strand eq '+' ) { $strand = 1 }
+  elsif ( $strand eq '-' ) { $strand = -1 }
+  else                     { $strand = 0 }
+
+  # ... and non-coding transcripts have cdsStart == cdsEnd.  We would
+  # like these to be stored as NULLs.
+  if ( $cdsStart == $cdsEnd ) {
+    undef($cdsStart);
+    undef($cdsEnd);
+  }
+
+  # ... and they use the same kind of "inbetween" coordinates as e.g.
+  # exonerate, so increment all start coordinates by one.
+  $txStart += 1;
+  $exonStarts = join( ',', map( { ++$_ } split( /,/, $exonStarts ) ) );
+  if ( defined($cdsStart) ) { $cdsStart += 1 }
+
+  # Cut off the last comma from $exonEnds, if it exists.  This is done
+  # for $exonStarts already (above).
+  if ( substr( $exonEnds, -1, 1 ) eq ',' ) { chop($exonEnds) }
+
+  # TODO
+  # set various record fields
+  # need to determine taxon ID and possibly extend Record class
+  # to include additional attributes which are parsed here
+  #
+  $record->id($name);
+  $name =~ s/\.\d$//;
+  $record->gene_name($name);
+  $record->display_label($name);
+  
+  # my %xref = ( 'accession'  => $name,
+  # 	       'chromosome' => $chrom,
+  # 	       'strand'     => $strand,
+  # 	       'txStart'    => $txStart,
+  # 	       'txEnd'      => $txEnd,
+  # 	       'cdsStart'   => $cdsStart,
+  # 	       'cdsEnd'     => $cdsEnd,
+  # 	       'exonStarts' => $exonStarts,
+  # 	       'exonEnds'   => $exonEnds );
+
+  # add xrefs to protein IDs
+  foreach my $protein_id (@protein_ids) {
+    my $source;
+    if ($source =~ /^[A-Z0-9]{6}$/) {
+      $source = 'UniProt';
+    } elsif ($source =~ /^ENS/) {
+      $source = 'Ensembl';
+    } else {
+      Bio::EnsEMBL::Mongoose::IOException->throw ("Unknown source for protein ID: $protein_id")
+    }
+    $record->add_xref(Bio::EnsEMBL::Mongoose::Persistence::RecordXref->new(source => $source, creator => 'UCSC', id => $protein_id));
+  }
+  
   return 1;
 }
 

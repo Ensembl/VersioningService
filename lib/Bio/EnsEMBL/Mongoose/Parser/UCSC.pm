4a323d02788771771517ae852d9412b996963861
Alessandro Vullo <avullo@ebi.ac.uk>
2016-12-19 11:13:16 +0000

    [ENSCORESW-2072]. UCSC parser, skeleton.

---
+++ lib/Bio/EnsEMBL/Mongoose/Parser/UCSC.pm
@@ -0,0 +1,61 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 NAME
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+package Bio::EnsEMBL::Mongoose::Parser::UCSC;
+
+use Moose;
+
+use Bio::EnsEMBL::Mongoose::IOException;
+use Bio::EnsEMBL::Mongoose::Persistence::Record;
+use Bio::EnsEMBL::Mongoose::Persistence::RecordXref;
+
+with 'Bio::EnsEMBL::Mongoose::Parser::Parser', 'MooseX::Log::Log4perl';
+
+sub read_record {
+  my $self = shift;
+  $self->clear_record;
+  my $record  = $self->record;
+  
+  my $fh = $self->source_handle;
+  my $content = <$fh>;
+  return unless $content; # skip if empty record
+    
+  chomp($content);
+  $content =~ s/\s*$//;
+
+  # my ($antibody, $antibody_id, $ensembl_peptide_id, $link) = split /,/, $content;
+  # Bio::EnsEMBL::Mongoose::IOException->throw ('Insufficient data in HPA line: ' . $content)
+  #     unless $antibody and $antibody_id and $ensembl_peptide_id;
+  # Bio::EnsEMBL::Mongoose::IOException->throw ("Wrong Ensembl peptide ID in HPA line: $ensembl_peptide_id")
+  #     if $ensembl_peptide_id !~ /^ENSP0{5}[0-9]{6}/;
+
+  # $record->id($antibody_id);
+  # $record->display_label($antibody);
+  # $record->add_xref(Bio::EnsEMBL::Mongoose::Persistence::RecordXref->new(source => 'Ensembl', creator => 'HPA', id => $ensembl_peptide_id));
+
+  return 1;
+}
+
+__PACKAGE__->meta->make_immutable;

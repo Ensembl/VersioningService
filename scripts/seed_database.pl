cfb52f8fb440415aca4bbc3f0788d767582917af
Alessandro Vullo <avullo@ebi.ac.uk>
2017-01-19 16:25:32 +0000

    [ENSCORESW-2083]. Enabling JGI source.

--- scripts/seed_database.pl
+++ scripts/seed_database.pl
@@ -39,5 +39,6 @@ $broker->add_new_source('MiRBase','MiRBase',1,'Bio::EnsEMBL::Versioning::Pipelin
 $broker->add_new_source('DBASS','DBASS',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::DBASS','Bio::EnsEMBL::Mongoose::Parser::DBASS');
 $broker->add_new_source('RGD','RGD',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::RGD','Bio::EnsEMBL::Mongoose::Parser::RGD');
 $broker->add_new_source('Xenbase','Xenbase',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::Xenbase','Bio::EnsEMBL::Mongoose::Parser::Xenbase');
+$broker->add_new_source('JGI','JGI',1,'Bio::EnsEMBL::Versioning::Pipeline::Downloader::JGI','Bio::EnsEMBL::Mongoose::Parser::JGI');
 
 print "Created basic Xref working set of sources. Now enable the update pipeline to allow it to download the first copy of each source\n";

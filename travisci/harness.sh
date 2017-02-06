5a6dd4bd00f490a8cfb1568df1bf8e92ad0b61b8
Alessandro Vullo <avullo@ebi.ac.uk>
2017-02-06 11:41:05 +0000

    [ENSCORESW-2158]. Required dependencies and configuration file.

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -2,13 +2,14 @@
 
 ENSDIR="${ENSDIR:-$PWD}"
 
-export PERL5LIB=$ENSDIR/bioperl-live:$ENSDIR/ensembl/modules:$ENSDIR/ensembl-hive/modules:$ENSDIR/ensembl-taxonomy/modules
+export PERL5LIB=$ENSDIR/bioperl-live:$ENSDIR/ensembl/modules:$ENSDIR/ensembl-io/modules:$ENSDIR/ensembl-hive/modules:$ENSDIR/ensembl-taxonomy/modules
 export TEST_AUTHOR=$USER
 export TEST_POD=1
 export FUSEKI_HOME=$ENSDIR/apache-jena-fuseki-2.4.1/
 export PATH=$FUSEKI_HOME/bin:$PATH # allow to find s-put for fuseki integration tests
 
 cp conf/databases.conf.example conf/databases.conf
+cp conf/test.conf.example conf/test.conf
 
 echo "Running test suite"
 prove -vl

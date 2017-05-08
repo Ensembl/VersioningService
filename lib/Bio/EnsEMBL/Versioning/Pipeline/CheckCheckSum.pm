5b61898d890847089ec14abad906ed3cd2f94ac3
Kieron Taylor <ktaylor@ebi.ac.uk>
2017-05-08 14:35:44 +0100

    Named arguments for work_with_run()

--- lib/Bio/EnsEMBL/Versioning/Pipeline/CheckCheckSum.pm
+++ lib/Bio/EnsEMBL/Versioning/Pipeline/CheckCheckSum.pm
@@ -104,7 +104,7 @@ sub search_source_by_checksum {
   my $path = $self->param('output_path');
   my $fh = IO::File->new($path.$source.'_checksum.ttl','w') or throw("Failed to open ${source}_checksum.ttl for writing");
   my $writer = Bio::EnsEMBL::Mongoose::Serializer::RDF->new(handle => $fh, config_file => $self->param('broker_conf'));
-  $indexer->work_with_run($source,$run_id);
+  $indexer->work_with_run(source => $source,run_id => $run_id);
   
   foreach my $ens_id (keys %$checksum_hash) {
     $indexer->query(

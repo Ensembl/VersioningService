cb5be0382477490865258167658f5d6699b9fcce
Kieron Taylor <ktaylor@ebi.ac.uk>
2017-05-17 16:22:27 +0100

    Explicitly exclude mouse strains from xref pipeline.

--- lib/Bio/EnsEMBL/Versioning/Pipeline/PipeConfig/Xref_pipeline_conf.pm
+++ lib/Bio/EnsEMBL/Versioning/Pipeline/PipeConfig/Xref_pipeline_conf.pm
@@ -46,7 +46,7 @@ sub default_options {
     pipeline_name => 'xref_pipeline_'.time,
     species => [],
     division => [],
-    antispecies =>[],
+    antispecies =>[qw/mus_musculus_129s1svimj mus_musculus_aj mus_musculus_akrj mus_musculus_balbcj mus_musculus_c3hhej mus_musculus_c57bl6nj mus_musculus_casteij mus_musculus_cbaj mus_musculus_dba2j mus_musculus_fvbnj mus_musculus_lpj mus_musculus_nodshiltj mus_musculus_nzohlltj mus_musculus_pwkphj mus_musculus_wsbeij mus_spretus_spreteij/],
     run_all => 0, #always run every species
     ## Set to '1' for eg! run
     #   default => OFF (0)

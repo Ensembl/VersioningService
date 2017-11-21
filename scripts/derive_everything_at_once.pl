25a2ea86d42302a116e471e9060ad6aa1aa66393
Kieron Taylor <ktaylor@ebi.ac.uk>
2017-11-21 14:15:01 +0000

    Other Ensembl IDs get swept into the network. We can't filter solely on the original ID.

--- scripts/derive_everything_at_once.pl
+++ scripts/derive_everything_at_once.pl
@@ -114,11 +114,9 @@ foreach my $type (qw/Gene Transcript Translation/) {
     my @labels;
     foreach my $match (@$matches) {
       next if (
-           $match->{xref_label} eq $feature->stable_id 
-        && ( $match->{xref_source} eq 'http://rdf.ebi.ac.uk/resource/ensembl/'
+        $match->{xref_source} eq 'http://rdf.ebi.ac.uk/resource/ensembl/'
           || $match->{xref_source} eq 'http://rdf.ebi.ac.uk/resource/ensembl.transcript/'
           || $match->{xref_source} eq 'http://rdf.ebi.ac.uk/resource/ensembl.protein/'
-          )
         ); # We don't need to revisit any Ensembl ID. Sometimes Ensembl IDs are externally used so we have to check to the source too
       my $match_set = $reasoner->get_detail_of_uri($match->{uri});
 
@@ -131,7 +129,7 @@ foreach my $type (qw/Gene Transcript Translation/) {
         printf "Failed to resolve %s into external_db for id %s\n",$match_set->[0]->{source},$match_set->[0]->{id};
         next;
       } else {
-        printf "Storing direct xref %s:%s with label %s into Ensembl core DB\n",$root_source,$root_id,$match_set->[0]->{id};
+        printf "Storing direct xref %s:%s with label %s into Ensembl core DB\n",$match_set->[0]->{source},$match_set->[0]->{id},$match_set->[0]->{id};
       }
 
       # Insert as Direct Xref
